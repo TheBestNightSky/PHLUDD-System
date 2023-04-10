c2fc653ec1e75fc0684b165c540c61b7771302a9
Zachary White <shadowcraft3001@gmail.com>
2023-04-10 05:55:15 -0300

    Implement better method for obtaining local IP adress of server

--- web/server.py
+++ web/server.py
@@ -208,9 +208,22 @@ Command.register("deleteAccount", 1, delete_account, usage, description)
 
 #### END BUILT IN COMMAND DEFINITIONS ####
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 #Socket server
 PORT = 5050
-HOST = socket.gethostbyname(socket.gethostname())
+HOST = get_local_ip()
 HOST_EXTERNAL = requests.get('https://api.ipify.org').content.decode('utf8')
 ADDRESS = (HOST, PORT)
 
